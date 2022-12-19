7705c76e2444059c7b3139f25b4c813d111e635e
muhammad bilal <muhammadbilal@muhammads-MacBook-Pro.local>
2022-12-19 23:53:43 +0500

    xyz

--- deploy_scripts/run.sh
+++ deploy_scripts/run.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
 cd /home/ec2-user/node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install --lts
+nvm install 16
 npm install
 npm start
