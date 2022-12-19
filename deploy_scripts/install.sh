71a304123b9a22c4824e1cfbe2f8036bbce65542
muhammad bilal <muhammadbilal@muhammads-MacBook-Pro.local>
2022-12-19 22:01:26 +0500

    Add spaces

--- deploy_scripts/install.sh
+++ deploy_scripts/install.sh
@@ -1,7 +1,10 @@
 # add nodejs to yum
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+
 . ~/.nvm/nvm.sh
+
 nvm install --lts
+
 nvm install 16
  
 # install pm2 module globaly
