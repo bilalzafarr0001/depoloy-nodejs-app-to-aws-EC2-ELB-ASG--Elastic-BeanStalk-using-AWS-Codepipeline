0f68bf5f28af6c3d4fe1ca889ece7c037afc3a4d
muhammad bilal <muhammadbilal@muhammads-MacBook-Pro.local>
2022-12-19 23:20:45 +0500

    xyz

--- deploy_scripts/run.sh
+++ deploy_scripts/run.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env bash
-
 cd /home/ec2-user/node
-# add nodejs to yum
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-
-. ~/.nvm/nvm.sh
-
-nvm install --lts
-
-nvm install 16
-
 npm install
 npm start
