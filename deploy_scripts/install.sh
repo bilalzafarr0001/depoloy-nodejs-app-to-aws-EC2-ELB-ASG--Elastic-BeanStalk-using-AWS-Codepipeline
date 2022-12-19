51699bceac1e806b43b7e7bfb260fcf2668d5016
muhammad bilal <muhammadbilal@muhammads-MacBook-Pro.local>
2022-12-20 02:10:41 +0500

    Comment node installation in install.sh file

--- deploy_scripts/install.sh
+++ deploy_scripts/install.sh
@@ -1,15 +1,15 @@
-# add nodejs to yum
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+# # add nodejs to yum
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
-. ~/.nvm/nvm.sh
+# . ~/.nvm/nvm.sh
 
-nvm install --lts
+# nvm install --lts
 
-nvm install 16
+# nvm install 16
  
-# install pm2 module globaly
-npm install -g pm2
-pm2 update
+# # install pm2 module globaly
+# npm install -g pm2
+# pm2 update
 
 # delete existing bundle
 cd /home/ec2-user
