381fcf11908fd3a3d6d59a13714f6e783972d551
muhammad bilal <muhammadbilal@muhammads-MacBook-Pro.local>
2022-12-20 22:41:37 +0500

    revert changes

--- deploy_scripts/install.sh
+++ deploy_scripts/install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
-. ~/.nvm/nvm.sh
+# . ~/.nvm/nvm.sh
 
-nvm install --lts
+# nvm install --lts
 
-nvm install 16
+# nvm install 16
  
 # install pm2 module globaly
 npm install 
