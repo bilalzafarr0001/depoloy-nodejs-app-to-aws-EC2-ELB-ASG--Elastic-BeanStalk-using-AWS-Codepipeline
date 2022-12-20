3c538a5575b613bbefb192b070b0f2f7459d0044
muhammad bilal <muhammadbilal@muhammads-MacBook-Pro.local>
2022-12-20 23:06:55 +0500

    revert changes

--- deploy_scripts/install.sh
+++ deploy_scripts/install.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
-# . ~/.nvm/nvm.sh
+. ~/.nvm/nvm.sh
 
-# nvm install --lts
+nvm install --lts
 
-# nvm install 16
+nvm install 16
  
 # install pm2 module globaly
 npm install 
 npm install -g pm2
 pm2 update
-
+cd /home/ec2-user/node
+# npm install
+npm start
 # delete existing bundle
-cd /home/ec2-user
+# cd /home/ec2-user
 # rm -rf node
--- deploy_scripts/run.sh
+++ deploy_scripts/run.sh
@@ -3,6 +3,6 @@
 # . ~/.nvm/nvm.sh
 # nvm install --lts
 # nvm install 16
-cd /home/ec2-user/node
-# npm install
-npm start
+# cd /home/ec2-user/node
+# # npm install
+# npm start
