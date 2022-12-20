213789c82eb83abca669111efc8ea5228845c1d6
Muhammad Bilal <muhammadbilal@nxb.com.pk>
2022-12-20 20:58:45 +0500

    Changes

--- deploy_scripts/run.sh
+++ deploy_scripts/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-# . ~/.nvm/nvm.sh
-# nvm install --lts
-# nvm install 12
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install --lts
+nvm install 16
 # cd /home/ec2-user/node
 # npm install
 npm start
