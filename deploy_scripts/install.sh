c53c10cb7a6268d253d51f24c3150aa84ce66bf6
Muhammad Bilal <muhammadbilal@nxb.com.pk>
2022-12-21 18:04:11 +0500

    opposite runs

--- deploy_scripts/install.sh
+++ deploy_scripts/install.sh
@@ -1,19 +1,12 @@
 #!/usr/bin/env bash
-
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-
 . ~/.nvm/nvm.sh
-
 nvm install --lts
-
 nvm install 16
- 
 # install pm2 module globaly
-sudo npm install 
-sudo npm install -g pm2
+npm install 
+npm install -g pm2
 pm2 update
-
-
 # delete existing bundle
 cd /home/ec2-user
-# rm -rf node
+rm -rf node
--- deploy_scripts/run.sh
+++ deploy_scripts/run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 cd /home/ec2-user/node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
