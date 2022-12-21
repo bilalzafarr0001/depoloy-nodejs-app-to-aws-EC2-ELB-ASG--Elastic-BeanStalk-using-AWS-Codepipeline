bc16fe99553bc43c07c4a3a01a8cb77fe002b242
Muhammad Bilal <muhammadbilal@nxb.com.pk>
2022-12-21 17:59:27 +0500

    opposite runs

--- deploy_scripts/run.sh
+++ deploy_scripts/run.sh
@@ -5,5 +5,5 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install --lts
 nvm install 16
-sudo npm install
-sudo npm start
+npm install
+npm start
